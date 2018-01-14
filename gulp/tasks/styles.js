ba1d3e96d355b59a15334a34a4de7ac852c19857
Michele Peghini <michelepeghini@gmail.com>
2018-01-15 00:07:33 +1100

    auto sprite generation

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssVars = require('postcss-simple-vars'),
-    cssNested = require('postcss-nested'),
-    cssImport = require('postcss-import'),
-    cssMixins = require('postcss-mixins');
+    postcss = require('gulp-postcss'), // post css
+    autoprefixer = require('autoprefixer'), // automatic css vendor prefixes
+    cssVars = require('postcss-simple-vars'), // css $variables
+    cssNested = require('postcss-nested'), // css nested selectors
+    cssImport = require('postcss-import'), // css @import
+    cssMixins = require('postcss-mixins'); // css @mixins
 
 gulp.task('styles', function() {
   //async functions must use return statement
   return gulp.src('./app/assets/styles/styles.css') //main file with all @import
-    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, autoprefixer])) //array of packages, ORDER MATTERS
-    .on('error', function(err) { //handles error gracefully and prevents "watch" task to stop
+    //array of packages, ORDER MATTERS
+    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, autoprefixer]))
+    //handles error gracefully and prevents "watch" task to stop
+    .on('error', function(err) {
       console.log(err.toString()); // print error in readable format
       this.emit('end'); // end task, but keeps "watch" task going
     })
-    .pipe(gulp.dest('./app/temp/styles')); // put everything in named folder, file name is same as src
+     // put everything in named folder, file name is same as src
+    .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
-
+require('./gulp/tasks/sprites');
 
 /* default: CLI> "gulp", no name required
  'taskName', function() {tasks}
