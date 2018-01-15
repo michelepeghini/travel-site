5ebdc1831352c1259a4037f0fc9753ddd57188cb
Michele Peghini <michelepeghini@gmail.com>
2018-01-15 16:34:42 +1100

    babel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,20 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+//var Person = require('./modules/Person');
 
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log("no debt");
+  };
+
+}
 alert("ABC 321");
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "orange");
 jane.greet();
-
+jane.payTaxes();
 $("h1").remove();
